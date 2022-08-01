b436a96a7a2f1d79219f9a6b492172a7352a013b
Matt <mattdanielbarnett@gmail.com>
2022-08-01 21:04:54 +0100

    Inital commit

---
+++ content/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="content/style.css">
+</head>    
+<body>
+
+<h1>mattdbarnett's PHP Essentials</h1>
+
+<h2>1. Basic Echo</h2>
+<?php
+echo "This is an echo.";
+?>
+
+</body>
+</html> 
