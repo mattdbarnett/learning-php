d6419ee1044b8b1ebee178caa93e0820a9b7cb53
Matt <mattdanielbarnett@gmail.com>
2022-08-08 19:21:13 +0100

    Added sections 2, 3 and 4

--- content/home.php
+++ content/home.php
@@ -12,5 +12,33 @@
 echo "This is an echo.";
 ?>
 
+<h2>2. Basic Data Types</h2>
+<?php
+$intvar = 5;
+$doublevar = 10.5;
+$boolvar = false;
+$strvar = "Text";
+
+echo "$intvar is an integer.<br>";
+echo "$doublevar is a double.<br>";
+echo ($boolvar ? 'true' : 'false') . " is a boolean.<br>";
+echo "$strvar is a string.<br>";
+?>
+
+<h2>3. Basic Arrays</h2>
+<?php 
+$basicItems = array("Item 1","Item 2","Item 3","Item 4");
+echo "Echos each item in \$basicItems:<br>";
+foreach($basicItems as $item){
+    echo $item . "<br>";
+}
+?>
+
+<h2>4. Associative Arrays </h2>
+<?php
+$assoItems=array("Matt"=>"21","George"=>"29","Greg"=>"43");
+echo "Echos the associated age of Matt:<br>";
+echo $assoItems['Matt'];
+?>
 </body>
 </html> 
